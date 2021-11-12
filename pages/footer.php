8538d67a531cfe9de70f0d318e20382b5becb3a7
Kweku <kwekuappiah11@gmail.com>
2021-11-12 11:19:38 +0000

    initial commit; footer added

---
+++ pages/footer.php
@@ -0,0 +1,128 @@
+
+
+
+  <footer class="cs-primary cente">
+	<div class="container">
+		<div class="row align-items-start">
+
+
+			<!-- Explore by status  -->
+			<div class="col">
+				<h4 class="mb-3 text-uppercase">Explore by status</h4>
+				<ul>
+					<li>
+						<a href="" class ="footer-link">Completed Projects</a>
+					</li>
+
+					<li>
+						<a href="" class ="footer-link">Ongoing Projects</a>
+					</li>
+
+					<li>
+						<a href="" class ="footer-link">Abandoned Projects</a>
+					</li>
+
+					<li>
+						<a href="" class ="footer-link">Pending Projects</a>
+					</li>
+
+					<br>
+
+					<li>
+						<a href="" class ="footer-link">Proposed Projects</a>
+					</li>
+				</ul>
+
+			</div>
+
+
+
+			<!-- Explore by region  -->
+			<div class="col">
+				<h4 class="mb-3 text-uppercase">Explore by region</h4>
+				<ul>
+				<li>
+						<a href="" class ="footer-link">Greater Accra Region</a>
+					</li>
+					<li>
+						<a href="" class ="footer-link">Ashanti Region</a>
+					</li>
+					<li>
+						<a href="" class ="footer-link">Central Region</a>
+					</li>
+					<li>
+						<a href="" class ="footer-link">Eastern Region</a>
+					</li>
+					<li>
+						<a href="" class ="footer-link">Northern Region</a>
+					</li>
+					<li>
+						<a href="" class ="footer-link">Western Region</a>
+					</li>
+					<li>
+						<a href="" class ="footer-link">Oti Region</a>
+					</li>
+
+
+
+				</ul>
+
+			</div>
+
+
+
+
+			<!-- Explore by ministry  -->
+			<div class="col">
+				<h4 class="mb-3 text-uppercase">Explore by ministry</h4>
+				<ul>
+
+					<li>
+						<a href="" class ="footer-link">Ministry of Education</a>
+					</li>
+
+					<li>
+						<a href="" class ="footer-link">Ministry of Health</a>
+					</li>
+
+					<li>
+						<a href="" class ="footer-link">Ministry of Transportation</a>
+					</li>
+
+					<li>
+						<a href="" class ="footer-link">Ministry of Information</a>
+					</li>
+				</ul>
+
+			</div>
+
+			<div class="col">
+				<h4 class="mb-3 text-uppercase">contact us</h4>
+				<p>
+
+					Ashesi University <br>
+					1 University Avenue <br>
+					Berekuso ER <br>
+					Ghana
+				</p>
+				<p>
+					Tel: +233 55 958 2518
+				</p>
+				<p>
+					kweku.acquaye@ashesi.edu.gh
+				</p>
+
+			</div>
+
+
+		</div>
+	</div>
+
+
+	<p class="text-center text-uppercase"> &copy; Copyright  2021 all rights reserved</p>
+	<p class="text-center"> Built and maintained by Duala Inc <a>Report a problem</a></p>
+
+
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
