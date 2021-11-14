8ae4bb7c28cf427b047cfcf70657ce2a88f2f06c
Kweku <kwekuappiah11@gmail.com>
2021-11-14 19:24:17 +0000

    got rid of file structure

--- functions/constants.php
+++
@@ -1,5 +0,0 @@
-<?php
-	define ('ROOT', $_SERVER['HTTPS']."/gcd/");
-	define("HOMEPAGE_PATH", ROOT."pages/");
-	define("CONTRACT_SEARCH_PATH", ROOT."pages/contract_search.php");
-?>
--- index.php
+++ index.php
@@ -8,8 +8,20 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+	<link rel="stylesheet" href=<?php echo "styles/main.css"?> type="text/css">
+	<link rel="stylesheet" href=<?php echo "styles/header.css"?> type="text/css">
+	<link rel="stylesheet" href=<?php echo "styles/footer.css"?> type="text/css">
+	<link rel="stylesheet" href=<?php echo "styles/auth_banner.css"?> type="text/css">
+
+
+
+
 	<title>Ghana Contracts Database</title>
-	<?php require_once "pages/header.php"; ?>
+
+	<?php
+
+	require_once "header.php"; ?>
 
 
 
@@ -131,5 +143,5 @@
 
 
 
-	<?php require_once "pages/footer.php" ?>
+	<?php require_once "footer.php" ?>
 </html>
