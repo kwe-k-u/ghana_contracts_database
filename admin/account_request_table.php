ef9247421220def964c4b87223dde89327878f00
Kweku <kwekuappiah11@gmail.com>
2021-12-09 23:36:49 +0000

    files for previous commit

---
+++ admin/account_request_table.php
@@ -0,0 +1,41 @@
+
+<!-- table  -->
+<section id="account_request_table">
+
+<table class="table table-hover table-bordered fixed-head">
+
+	<caption>List of All Requests</caption>
+	<h4>Account Requests</h4>
+	<thead>
+		<tr>
+			<th scope="col">ID</th>
+			<th scope="col">First name</th>
+			<th scope="col">Last Name</th>
+			<th scope="col">Email</th>
+			<th scope="col">Supervisor Name </th>
+			<th scope="col">Office Address</th>
+			<th scope="col">City</th>
+			<th scope="col">Zip</th>
+			<th scope="col">Status</th>
+			<th scope="col">Approve</th>
+			<th scope="col">Reject</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<th scope="row">1</th>
+			<td>Otto</td>
+			<td>Otto</td>
+			<td>Otto</td>
+			<td>Otto</td>
+			<td>Otto</td>
+			<td>Otto</td>
+			<td>Otto</td>
+			<td>Otto</td>
+				<td> <a class="btn btn-primary">Approve </a></td>
+				<td> <a class="btn btn-warning">Reject </a></td>
+		</tr>
+	</tbody>
+</table>
+
+</section>
---
+++ admin/password_table.php
@@ -0,0 +1,37 @@
+
+<!-- table  -->
+<section id="password_table"  style="display: none;">
+
+<table class="table table-hover table-bordered fixed-head">
+	<caption>List of All contracts</caption>
+	<thead>
+		<tr>
+			<th scope="col">Index</th>
+			<th scope="col">Last Name</th>
+			<th scope="col">Email</th>
+			<th scope="col">Supervisor Name</th>
+			<th scope="col">Office Address </th>
+			<th scope="col">City</th>
+			<th scope="col">Zip</th>
+			<th scope="col">COMPLETION DATE</th>
+			<th scope="col">Approve</th>
+			<th scope="col">Reject</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<th scope="row">1</th>
+			<td>Mark</td>
+			<td>Otto</td>
+			<td>Otto</td>
+			<td>Otto</td>
+			<td>Otto</td>
+			<td>Otto</td>
+			<td>Otto</td>
+				<td> <a href="insert_contract.php" class="btn btn-success">Approve </a></td>
+				<td> <a href="insert_contract.php" class="btn btn-warning">Reject </a></td>
+		</tr>
+	</tbody>
+</table>
+
+</section>
